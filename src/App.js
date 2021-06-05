7dcc163ea231db86c608b7dad60e64eb492dcca8
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-06-05 20:57:40 -0300

    learning localStorage and sessionStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import api from "./services/api";
 
@@ -7,8 +7,22 @@ function App() {
   const [username, setUsername] = useState("Andrew-2609");
   const [userData, setUserData] = useState({});
 
+  useEffect(() => {
+    // **using localStorage**
+    // const localStorageUserData = localStorage.getItem("@basic-react-app/gitHubUserData");
+
+    const sessionStorageData = sessionStorage.getItem("@basic-react-app/gitHubUserData");
+
+    setUserData(JSON.parse(sessionStorageData) || {});
+  }, []);
+
   async function getUserGitHubData() {
     const { data } = await api.get(username);
+
+    // **using localStorage**
+    // localStorage.setItem("@basic-react-app/gitHubUserData", JSON.stringify(data));
+
+    sessionStorage.setItem("@basic-react-app/gitHubUserData", JSON.stringify(data));
 
     setUserData(data);
   }
